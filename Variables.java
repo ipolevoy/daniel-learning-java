aea5a4d6bb953261ee04c9448969369941779fa1
Igor Polevoy <500941+ipolevoy@users.noreply.github.com>
2020-12-28 17:40:51 -0600

    simple variables example

---
+++ Variables.java
@@ -0,0 +1,34 @@
+
+
+public  class Variables{
+
+	public static void main(String [] args){
+
+
+
+		int x =	-400;
+
+		int y = x + 456; 
+	
+
+
+		if(y < 100) {
+			System.out.println("Cold");
+		} else {
+			System.out.println("Hot");
+		}
+
+		
+
+
+		System.out.println(y);
+
+	}
+
+
+
+
+
+
+
+}
