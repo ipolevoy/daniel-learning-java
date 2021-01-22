cef8e0bc7b511f658a59f611c5a7be79c91f7b1c
Igor Polevoy <500941+ipolevoy@users.noreply.github.com>
2021-01-21 18:11:16 -0600

    explain final, static, public  keyword

---
+++ src/access_modifiers/Fruit.java
@@ -0,0 +1,12 @@
+package access_modifiers;
+
+public class Fruit {
+
+    public static final double weight = 2.5d;
+
+    private String color = "red";
+
+    public String getColor() {
+        return color;
+    }
+}
---
+++ src/access_modifiers/Main.java
@@ -0,0 +1,14 @@
+package access_modifiers;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Fruit f = new Fruit();
+        System.out.println(f.getColor());
+
+        f.weight = 3.2d; // will not compile because we are changing a final variable
+
+        System.out.println(f.weight);
+
+    }
+}
