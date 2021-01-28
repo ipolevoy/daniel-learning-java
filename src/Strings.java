60fcf79598bad051b247093fad539f9d9776863b
Daniel Radunsky <dradunsky2042@gmail.com>
2021-01-28 18:12:26 -0500

    New String Practice File

---
+++ src/Strings.java
@@ -0,0 +1,33 @@
+class Strings {
+
+	public static void main (String [] args) {
+		
+		String str = "daniel";
+		String str1 = " and Michael";
+		String str2 = "daniel";
+		String concat = str + str1;
+		int num = 75;
+		double dbl = 34.24;
+		String spaces = " Hello and Hi and Welcome ";				
+		
+
+		System.out.println(str.charAt(3));
+		System.out.println(concat);
+		System.out.println(str.equals(str2) + " " + str.equals(str1));
+		System.out.println(str.equalsIgnoreCase(str2) + " " + str.equalsIgnoreCase(str1));
+		System.out.println(str1.toUpperCase());
+		System.out.println(spaces.trim());
+
+
+		String bruh = new String("Wowzers");
+		char[] array = bruh.toCharArray();
+		
+		for (char c: array) { 
+			System.out.println(array[5]);
+			System.out.println(c);
+		}
+			System.out.print(array[6] + array[5] + array[4] + array[3] + array[2] + array[1] + array[0]);
+		
+			
+	}		
+}		
