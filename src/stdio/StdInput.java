0cb30003f126821d9724b515bed2ffeed85dbc4f
Igor Polevoy <500941+ipolevoy@users.noreply.github.com>
2021-02-08 19:42:14 -0600

    fixed HW with new line characters

--- src/stdio/StdInput.java
+++ src/stdio/StdInput.java
@@ -1,45 +1,29 @@
-//package stdio;
+package stdio;
 
 import java.io.IOException;
 
 public class StdInput {
 
     public static void main(String[] args) throws IOException {
-	
+
         System.out.println("Type your first name here: ");
-	
-	StringBuffer sb = new StringBuffer();
-	StringBuffer bs = new StringBuffer();
-	
-	int x = System.in.read();
-	
-	while (x != 33) {
-		//System.out.print((char) x);
-		sb.append((char) x);
-		x = System.in.read();
-		}
-	System.out.println("Type your last name here: ");
-	
-	int y = System.in.read();
-	 while (y != 33) {
-                //System.out.print((char) y);
-                bs.append((char) y);
-                y = System.in.read();
-                }
-	System.out.println(sb.toString().trim() + " " + bs.toString().trim());
 
-		System.out.println(sb);
-		System.out.println(sb.toString().toUpperCase());
-//		System.out.println(sb.replace(3,7, "wdad"));
-		System.out.println(sb.insert(4, "jay"));
-	//x = System.in.read();
-       // System.out.println("you typed " + (char) x);
+        StringBuffer sb1 = new StringBuffer();
+        StringBuffer sb2 = new StringBuffer();
 
-/*	for(int i = 0; (char) System.in.read() != 'A'; i++){
+        char x = (char) System.in.read();
 
-		System.out.print((char) i);
-	}
-*/		
-//		System.out.print(sb);
-	}
+        while (x != '\n') {
+            sb1.append(x);
+            x = (char) System.in.read();
+        }
+        System.out.println("Type your last name here: ");
+
+        char y = (char) System.in.read();
+        while (y != '\n') {
+            sb2.append( y);
+            y = (char) System.in.read();
+        }
+        System.out.println("Your name is: "  + sb1 + " " + sb2);
+    }
 }
