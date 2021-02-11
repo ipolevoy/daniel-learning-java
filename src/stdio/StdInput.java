6265f0893772ac76096157d538eefc542d4e3eb8
Igor Polevoy <500941+ipolevoy@users.noreply.github.com>
2021-02-11 17:23:09 -0600

    fixing broken code

--- src/stdio/StdInput.java
+++ src/stdio/StdInput.java
@@ -8,34 +8,6 @@ public class StdInput {
 
         System.out.println("Type your first name here: ");
 
-	
-	StringBuffer sb = new StringBuffer();
-	StringBuffer bs = new StringBuffer();
-	
-	int x = System.in.read();
-	
-	while (x != 33) {
-		//System.out.print((char) x);
-		sb.append((char) x);
-		x = System.in.read();
-		}
-	System.out.println("Type your last name here: ");
-	
-	int y = System.in.read();
-	 while (y != 33) {
-                //System.out.print((char) y);
-                bs.append((char) y);
-                y = System.in.read();
-                }
-	System.out.println(sb.toString().trim() + " " + bs.toString().trim());
-
-		System.out.println(sb);
-		System.out.println(sb.toString().toUpperCase());
-
-		System.out.print((char) i);
-	}
-
-
         StringBuffer sb1 = new StringBuffer();
         StringBuffer sb2 = new StringBuffer();
 
@@ -54,6 +26,4 @@ public class StdInput {
         }
         System.out.println("Your name is: "  + sb1 + " " + sb2);
     }
-
 }
-
