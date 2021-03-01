fab835e39a621c4f1e7a89539e2b2d6496c73646
Igor Polevoy <500941+ipolevoy@users.noreply.github.com>
2021-03-01 17:53:02 -0600

    completed refactoring

--- src/stdio/UpdatedCalculator.java
+++ src/stdio/UpdatedCalculator.java
@@ -1,78 +1,84 @@
 package stdio;
 
 
-import java.lang.Math;
 import java.util.Scanner;
 
 public class UpdatedCalculator {
 
     private static final String OPERATORS = "+-/*";
+    private static final String PLUS = "+";
+    private static final String MINUS = "-";
+    private static final String MULT = "*";
+    private static final String DIVIDE = "/";
     private static final Scanner scanner = new Scanner(System.in);
+    private static final CalcMethods CM = new CalcMethods();
 
     public static void main(String args[]) {
-
         System.out.println("Welcome to the Java Calculator!" + '\n');
+        doWork();
+    }
 
-        String operand = "";
+    private static void doWork(){
 
-        CalcMethods cm = new CalcMethods();
         while (true) {
             String operator = getOperator();
-            System.out.println("Print first value: ");
-            double valFirst = getFirstValue();
-           System.out.println("Print second value: ");
-            double valSecond = getFirstValue();
-       
-            if (operator.indexOf("*") == 0) { 
-                System.out.print("The result is: " + cm.multiply(valFirst, valSecond));
-            } else if (operator.indexOf("/") == 0) {
-                System.out.print("The result is: " + cm.divide(valFirst, valSecond));
-            } else if (operator.indexOf("+") == 0) {
-                System.out.print("The result is: " + cm.add(valFirst, valSecond));
-            } else if (operator.indexOf("-") == 0) {
-                System.out.print("The result is: " + cm.subtract(valFirst, valSecond));
-            }
-
+            double valFirst = getValue("Print first value: ");
+            double valSecond = getValue("Print second value: ");
+            double result = calculate(operator, valFirst, valSecond);
+            System.out.println("The result is: " + result);
             System.out.print('\n' + "Do you want to do another calculation? (Enter Y or N): ");
             String boo = scanner.nextLine();
             if (boo.indexOf("N") == 0) {
                 break;
             }
-     }
-                        
+        }
     }
 
-        private static String getOperator() {
-            String operator;
-            while (true) {
-                System.out.println("List of possible operations: +, -, *, /");
-                System.out.print("Type of operation: ");
+    private static double calculate(String operator, double valFirst, double valSecond){
+        if (operator.indexOf(MULT) == 0) {
+            return CM.multiply(valFirst, valSecond);
+        } else if (operator.indexOf(DIVIDE) == 0) {
+            return CM.divide(valFirst, valSecond);
+        } else if (operator.indexOf(PLUS) == 0) {
+            return CM.add(valFirst, valSecond);
+        } else if (operator.indexOf(MINUS) == 0) {
+            return CM.subtract(valFirst, valSecond);
+        }else {
+            throw new RuntimeException("Bad operator");
+        }
+    }
 
-                operator = scanner.nextLine();
-                if (OPERATORS.contains(operator)) {
-                    break;
-                } else {
-                    System.out.println("Incorrect input type");
-                }
+    private static String getOperator() {
+        String operator;
+        while (true) {
+            System.out.println("List of possible operations: +, -, *, /");
+            System.out.print("Type of operation: ");
+
+            operator = scanner.nextLine();
+            if (OPERATORS.contains(operator)) {
+                break;
+            } else {
+                System.out.println("Incorrect input type");
             }
-            return operator;
+        }
+        return operator;
     }
 
 
-        private static double getFirstValue() {
-            String first;
-            double val1;
-            while (true) {
-   //             System.out.println("Print first value: ");
-                try {
-                    first = scanner.nextLine();
-                        val1 = Double.parseDouble(first);
-                    return val1;
-                }
-                catch (Exception e) {
-                    System.out.println("Oops! Input a number please!"); 
-                }
+    private static double getValue(String prompt ) {
+        System.out.println(prompt);
+        String first;
+        double val1;
+        while (true) {
+
+            try {
+                first = scanner.nextLine();
+                val1 = Double.parseDouble(first);
+                return val1;
+            } catch (Exception e) {
+                System.out.println("Oops! Input a number please!");
             }
+        }
     }
 }
 
