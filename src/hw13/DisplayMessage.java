10d8d21ce6838376bdd7af73df3a3a62918a3c89
your_username <your_emailid>
2021-04-26 17:40:18 -0400

    hw13 package

---
+++ src/hw13/DisplayMessage.java
@@ -0,0 +1,15 @@
+package hw13;
+
+public class DisplayMessage implements Runnable {
+   private String message;
+   
+   public DisplayMessage(String message) {
+      this.message = message;
+   }
+   
+   public void run() {
+      while(true) {
+         System.out.println(message);
+      }
+   }
+}
